c61123264a7afcc47748f8bf8ad2a56c2dae8f68
LangdalP <peder.langdal@gmail.com>
2015-12-14 13:14:32 +0100

    More cleaning up

--- go_timer.py
+++ go_timer.py
@@ -1,8 +1,10 @@
 import multiprocessing
-from bomb_timer_gui import TimerGui
-from gamestate_listener import ListenerWrapper
+from gui.bomb_timer_gui import TimerGui
+from listener.gamestate_listener import ListenerWrapper
 
 if __name__ == "__main__":
+    # We use a message queue so that the gui can consume messages
+    # created by the gamestate listener
     queue = multiprocessing.Queue()
     gui = TimerGui(queue)
     listener = ListenerWrapper(queue)
